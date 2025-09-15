d0a6711078e1516ea11c88821069c1da6029852d
Sebastian Korva <sebastian.korva@centria.fi>
2025-09-15 12:23:29 +0300

    fist commit

---
+++ harj2.py
@@ -0,0 +1,12 @@
+def CalculateSum(a,b):
+    return a + b
+
+def CalculateSubstraction(a,b):
+    return a - b
+
+def CalculateMultiplication(a,b):
+    return a * b
+
+def CalculateDivision(a,b):
+    return a/b
+
---
+++ harj3.py
@@ -0,0 +1,14 @@
+def Hello():
+    print("Kerro Nimesi: ")
+    name = input()
+    print("Hei " + name + "!")
+
+def YourAge():
+    print("Kuinka vanha olet: ")
+    age = input()
+    print("Olet " + age + " vuotta vanha!")
+
+def AskFavoriteColor():
+    print("Mikä on lempivärisi: ")
+    color = input()
+    print("Lempivärisi on " + color + "!")
